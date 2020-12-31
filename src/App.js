07afd7de8bb1a337ff586d3bab05b5e05d83c6ae
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-30 21:45:43 -0800

    Add pics, shuffle array

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      array: this.getArray()
+    };
+  }
+
+  getArray() {
+    let arr = [];
+    for (let i = 1; i <= 10; i++) {
+      arr.push(i);
+    }
+    return this.shuffle(arr);
+  }
+
+  shuffle(array) {
+    // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array  shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  render() {
+    const arr = JSON.stringify(this.state.array);
+    return (
+      <div className='App'>
+        <p>{arr}</p>
+        hiiii
+      </div>
+    );
+  }
 }
 
 export default App;
